e91c984e7655f64be6a8d1da28f57aea39f2e792
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2020-11-29 14:44:15 +0100

    Modified the product buttons to adapt different screens.
    Added style media querries for different screens.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,11 @@
 import React from 'react'
+//import image from '../images.jpg'
 
 const Login = () => {
     return (
         <div>
             <h4>Here you sign-in</h4>
+            <img style={{objectFit:"contain", width:"30%", height:"40px"}} src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
         </div>
     )
 }
